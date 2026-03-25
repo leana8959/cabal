bf0081fb49541f1ece6f619b3ca54699ad9446c0
Léana 江 <leana.jiang+git@icloud.com>
2026-03-25 18:42:42 +0100

    implement Ann variant of HasBuildInfoWith class

--- Cabal-syntax/src/Distribution/Types/BuildInfo/Lens.hs
+++ Cabal-syntax/src/Distribution/Types/BuildInfo/Lens.hs
@@ -28,7 +28,7 @@ import Prelude ()
 import Distribution.Compiler (PerCompilerFlavor)
 import Distribution.ModuleName (ModuleName)
 import Distribution.Types.BuildInfo (BuildInfo, BuildInfoWith)
-import Distribution.Types.Dependency (Dependency)
+import Distribution.Types.Dependency (Dependency, DependencyWith)
 import Distribution.Types.ExeDependency (ExeDependency)
 import Distribution.Types.LegacyExeDependency (LegacyExeDependency)
 import Distribution.Types.Mixin (Mixin)
@@ -184,7 +184,7 @@ class HasBuildInfoWith mod a | a -> mod where
   customFieldsBI :: (HasBuildInfoWith mod (BuildInfoWith mod)) => Lens' a [(String, String)]
   customFieldsBI = buildInfo @mod . customFieldsBI @mod
 
-  targetBuildDepends :: (HasBuildInfoWith mod (BuildInfoWith mod)) => Lens' a [Dependency]
+  targetBuildDepends :: (HasBuildInfoWith mod (BuildInfoWith mod)) => Lens' a [DependencyWith mod]
   targetBuildDepends = buildInfo @mod . targetBuildDepends @mod
 
   mixins :: (HasBuildInfoWith mod (BuildInfoWith mod)) => Lens' a [Mixin]
@@ -340,6 +340,154 @@ instance HasBuildInfoWith Mod.Bare (BuildInfoWith Mod.Bare) where
 
 type HasBuildInfos = HasBuildInfoWith Mod.Bare
 type HasBuildInfosAnn = HasBuildInfoWith Mod.Ann
+
+instance HasBuildInfoWith Mod.Ann (BuildInfoWith Mod.Ann) where
+  buildInfo = id
+  {-# INLINE buildInfo #-}
+
+  buildable f s = fmap (\x -> s{T.buildable = x}) (f (T.buildable s))
+  {-# INLINE buildable #-}
+
+  buildTools f s = fmap (\x -> s{T.buildTools = x}) (f (T.buildTools s))
+  {-# INLINE buildTools #-}
+
+  buildToolDepends f s = fmap (\x -> s{T.buildToolDepends = x}) (f (T.buildToolDepends s))
+  {-# INLINE buildToolDepends #-}
+
+  cppOptions f s = fmap (\x -> s{T.cppOptions = x}) (f (T.cppOptions s))
+  {-# INLINE cppOptions #-}
+
+  asmOptions f s = fmap (\x -> s{T.asmOptions = x}) (f (T.asmOptions s))
+  {-# INLINE asmOptions #-}
+
+  cmmOptions f s = fmap (\x -> s{T.cmmOptions = x}) (f (T.cmmOptions s))
+  {-# INLINE cmmOptions #-}
+
+  ccOptions f s = fmap (\x -> s{T.ccOptions = x}) (f (T.ccOptions s))
+  {-# INLINE ccOptions #-}
+
+  cxxOptions f s = fmap (\x -> s{T.cxxOptions = x}) (f (T.cxxOptions s))
+  {-# INLINE cxxOptions #-}
+
+  jsppOptions f s = fmap (\x -> s{T.jsppOptions = x}) (f (T.jsppOptions s))
+  {-# INLINE jsppOptions #-}
+
+  ldOptions f s = fmap (\x -> s{T.ldOptions = x}) (f (T.ldOptions s))
+  {-# INLINE ldOptions #-}
+
+  hsc2hsOptions f s = fmap (\x -> s{T.hsc2hsOptions = x}) (f (T.hsc2hsOptions s))
+  {-# INLINE hsc2hsOptions #-}
+
+  pkgconfigDepends f s = fmap (\x -> s{T.pkgconfigDepends = x}) (f (T.pkgconfigDepends s))
+  {-# INLINE pkgconfigDepends #-}
+
+  frameworks f s = fmap (\x -> s{T.frameworks = x}) (f (T.frameworks s))
+  {-# INLINE frameworks #-}
+
+  extraFrameworkDirs f s = fmap (\x -> s{T.extraFrameworkDirs = x}) (f (T.extraFrameworkDirs s))
+  {-# INLINE extraFrameworkDirs #-}
+
+  asmSources f s = fmap (\x -> s{T.asmSources = x}) (f (T.asmSources s))
+  {-# INLINE asmSources #-}
+
+  cmmSources f s = fmap (\x -> s{T.cmmSources = x}) (f (T.cmmSources s))
+  {-# INLINE cmmSources #-}
+
+  cSources f s = fmap (\x -> s{T.cSources = x}) (f (T.cSources s))
+  {-# INLINE cSources #-}
+
+  cxxSources f s = fmap (\x -> s{T.cxxSources = x}) (f (T.cxxSources s))
+  {-# INLINE cxxSources #-}
+
+  jsSources f s = fmap (\x -> s{T.jsSources = x}) (f (T.jsSources s))
+  {-# INLINE jsSources #-}
+
+  hsSourceDirs f s = fmap (\x -> s{T.hsSourceDirs = x}) (f (T.hsSourceDirs s))
+  {-# INLINE hsSourceDirs #-}
+
+  otherModules f s = fmap (\x -> s{T.otherModules = x}) (f (T.otherModules s))
+  {-# INLINE otherModules #-}
+
+  virtualModules f s = fmap (\x -> s{T.virtualModules = x}) (f (T.virtualModules s))
+  {-# INLINE virtualModules #-}
+
+  autogenModules f s = fmap (\x -> s{T.autogenModules = x}) (f (T.autogenModules s))
+  {-# INLINE autogenModules #-}
+
+  defaultLanguage f s = fmap (\x -> s{T.defaultLanguage = x}) (f (T.defaultLanguage s))
+  {-# INLINE defaultLanguage #-}
+
+  otherLanguages f s = fmap (\x -> s{T.otherLanguages = x}) (f (T.otherLanguages s))
+  {-# INLINE otherLanguages #-}
+
+  defaultExtensions f s = fmap (\x -> s{T.defaultExtensions = x}) (f (T.defaultExtensions s))
+  {-# INLINE defaultExtensions #-}
+
+  otherExtensions f s = fmap (\x -> s{T.otherExtensions = x}) (f (T.otherExtensions s))
+  {-# INLINE otherExtensions #-}
+
+  oldExtensions f s = fmap (\x -> s{T.oldExtensions = x}) (f (T.oldExtensions s))
+  {-# INLINE oldExtensions #-}
+
+  extraLibs f s = fmap (\x -> s{T.extraLibs = x}) (f (T.extraLibs s))
+  {-# INLINE extraLibs #-}
+
+  extraLibsStatic f s = fmap (\x -> s{T.extraLibsStatic = x}) (f (T.extraLibsStatic s))
+  {-# INLINE extraLibsStatic #-}
+
+  extraGHCiLibs f s = fmap (\x -> s{T.extraGHCiLibs = x}) (f (T.extraGHCiLibs s))
+  {-# INLINE extraGHCiLibs #-}
+
+  extraBundledLibs f s = fmap (\x -> s{T.extraBundledLibs = x}) (f (T.extraBundledLibs s))
+  {-# INLINE extraBundledLibs #-}
+
+  extraLibFlavours f s = fmap (\x -> s{T.extraLibFlavours = x}) (f (T.extraLibFlavours s))
+  {-# INLINE extraLibFlavours #-}
+
+  extraDynLibFlavours f s = fmap (\x -> s{T.extraDynLibFlavours = x}) (f (T.extraDynLibFlavours s))
+  {-# INLINE extraDynLibFlavours #-}
+
+  extraLibDirs f s = fmap (\x -> s{T.extraLibDirs = x}) (f (T.extraLibDirs s))
+  {-# INLINE extraLibDirs #-}
+
+  extraLibDirsStatic f s = fmap (\x -> s{T.extraLibDirsStatic = x}) (f (T.extraLibDirsStatic s))
+  {-# INLINE extraLibDirsStatic #-}
+
+  includeDirs f s = fmap (\x -> s{T.includeDirs = x}) (f (T.includeDirs s))
+  {-# INLINE includeDirs #-}
+
+  includes f s = fmap (\x -> s{T.includes = x}) (f (T.includes s))
+  {-# INLINE includes #-}
+
+  autogenIncludes f s = fmap (\x -> s{T.autogenIncludes = x}) (f (T.autogenIncludes s))
+  {-# INLINE autogenIncludes #-}
+
+  installIncludes f s = fmap (\x -> s{T.installIncludes = x}) (f (T.installIncludes s))
+  {-# INLINE installIncludes #-}
+
+  options f s = fmap (\x -> s{T.options = x}) (f (T.options s))
+  {-# INLINE options #-}
+
+  profOptions f s = fmap (\x -> s{T.profOptions = x}) (f (T.profOptions s))
+  {-# INLINE profOptions #-}
+
+  sharedOptions f s = fmap (\x -> s{T.sharedOptions = x}) (f (T.sharedOptions s))
+  {-# INLINE sharedOptions #-}
+
+  profSharedOptions f s = fmap (\x -> s{T.profSharedOptions = x}) (f (T.profSharedOptions s))
+  {-# INLINE profSharedOptions #-}
+
+  staticOptions f s = fmap (\x -> s{T.staticOptions = x}) (f (T.staticOptions s))
+  {-# INLINE staticOptions #-}
+
+  customFieldsBI f s = fmap (\x -> s{T.customFieldsBI = x}) (f (T.customFieldsBI s))
+  {-# INLINE customFieldsBI #-}
+
+  targetBuildDepends f s = fmap (\x -> s{T.targetBuildDepends = x}) (f (T.targetBuildDepends s))
+  {-# INLINE targetBuildDepends #-}
+
+  mixins f s = fmap (\x -> s{T.mixins = x}) (f (T.mixins s))
+  {-# INLINE mixins #-}
 
 class HasBuildInfosWith mod a where
   traverseBuildInfos :: Traversal' a (BuildInfoWith mod)
