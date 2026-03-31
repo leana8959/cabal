f138d5dfcc43b0cd92f9ca5995802dccb13e96d9
Léana 江 <leana.jiang+git@icloud.com>
2026-03-31 11:14:35 +0200

    make testcases names more descriptive

--- Cabal-tests/tests/ParserTests.hs
+++ Cabal-tests/tests/ParserTests.hs
@@ -184,14 +184,14 @@ parsecPrettyTests :: TestTree
 parsecPrettyTests = testGroup "parsec pretty roundtrip" $
   [ CabalSpecV1_0 .. ] <&> \specVer -> testGroup (show specVer) $
     optionals (specVer >= CabalSpecV2_0)
-      [ parsecPrettyTest @DependencyAnn specVer "Dependency" "text   == 1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text >   1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text >=   1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text<   1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text  <=  1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text ^>=   1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text ^>=   1 ||    >  2"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text (^>=   1 ||    >  2) && == 3"
+      [ parsecPrettyTest @DependencyAnn specVer "Dependency ==" "text   == 1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency >" "text >   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency >=" "text >=   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency <" "text<   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency <=" "text  <=  1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency ^>=" "text ^>=   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency ||" "text ^>=   1 ||    >  2"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency || and &&" "text (^>=   1 ||    >  2) && == 3"
       ]
   where
     optionals cond ifTrue = if cond then ifTrue else []
