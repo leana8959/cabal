e6384bd7c2a4eafefbe7996552df9a04ab433c77
Léana 江 <leana.jiang+git@icloud.com>
2026-03-31 09:48:43 +0200

    test entirety of cabal dependency specification

--- Cabal-tests/tests/ParserTests.hs
+++ Cabal-tests/tests/ParserTests.hs
@@ -184,8 +184,14 @@ parsecPrettyTests :: TestTree
 parsecPrettyTests = testGroup "parsec pretty roundtrip" $
   [ CabalSpecV1_0 .. ] <&> \specVer -> testGroup (show specVer) $
     optionals (specVer >= CabalSpecV2_0)
-      [ parsecPrettyTest @DependencyAnn specVer "Dependency" "text   ^>=   1"
-      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text  ==  1"
+      [ parsecPrettyTest @DependencyAnn specVer "Dependency" "text   == 1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text >   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text >=   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text<   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text  <=  1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text ^>=   1"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text ^>=   1 ||    >  2"
+      , parsecPrettyTest @DependencyAnn specVer "Dependency" "text (^>=   1 ||    >  2) && == 3"
       ]
   where
     optionals cond ifTrue = if cond then ifTrue else []
