07b9039d5e0c15656f8716029e6fea9517171c94
Léana 江 <leana.jiang+git@icloud.com>
2026-06-23 19:32:13 +0200

    remove todos

--- Cabal-syntax/src/Distribution/Annotation.hs
+++ Cabal-syntax/src/Distribution/Annotation.hs
@@ -8,7 +8,7 @@ import Distribution.Fields.Field
 
 import Distribution.Parsec
 
--- TODO(leana8959): We can label this with a position range so it looks like lsp-style edits, and modifications will mean preforming edits.
+-- NOTE(leana8959): We can label this with a position range so it looks like lsp-style edits, and modifications will mean preforming edits.
 data ExactAnn
   = ExactRepr BS.ByteString
   | IsInserted
@@ -17,11 +17,9 @@ data ExactAnn
 data SrcSpan = MkSrcSpan {-# UNPACK #-} !Position {-# UNPACK #-} !Position
   deriving (Show)
 
--- TODO(leana8959): refine type
 data Annotated a = MkAnnotated [Comment Position] ExactAnn (Located a)
   deriving (Show)
 
--- NOTE(leana8959): Move Located to its own module, here's a cycle
 data AnnotatedList a = MkAnnotatedList [Comment Position] ExactAnn [Located a]
   deriving (Show)
 
