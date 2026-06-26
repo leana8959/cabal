fb8d41234b83539bc61cb3b23a2606f7cb69ff3e
Léana 江 <leana.jiang+git@icloud.com>
2026-06-26 10:56:00 +0200

    add helpers to read from typed fields

--- Cabal-syntax/src/Distribution/Fields/Field/Typed.hs
+++ Cabal-syntax/src/Distribution/Fields/Field/Typed.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Distribution.Fields.Field.Typed where
 
 import Prelude ()
@@ -19,3 +20,15 @@ data TField ann
   | MkBuildToolsTField !(Name ann) (AnnotatedList LegacyExeDependency)
   | MkTSection !(Name ann) [SectionArg ann] [TField ann]
   deriving (Show)
+
+getCabalVersionField :: [TField ann] -> [(Name ann, Annotated CabalSpecVersion)]
+getCabalVersionField = mapMaybe $ \case { (MkCabalVersionTField name x) -> Just (name, x); _ -> Nothing; }
+
+getTargetBuildDependsField :: [TField ann] -> [(Name ann, AnnotatedList Dependency)]
+getTargetBuildDependsField = mapMaybe $ \case { (MkTargetBuildDependsTField name x) -> Just (name, x); _ -> Nothing; }
+
+getPkgVersionField :: [TField ann] -> [(Name ann, Annotated Version)]
+getPkgVersionField = mapMaybe $ \case { (MkPkgVersionTField name x) -> Just (name, x); _ -> Nothing; }
+
+getBuildToolsField :: [TField ann] -> [(Name ann, AnnotatedList LegacyExeDependency)]
+getBuildToolsField = mapMaybe $ \case { (MkBuildToolsTField name x) -> Just (name, x); _ -> Nothing; }
