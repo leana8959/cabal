d8c5790211ef4b007c242c320758d2206e99b5fd
Léana 江 <leana.jiang+git@icloud.com>
2026-06-30 10:51:43 +0200

    Reapply "try to implement parsec"
    This reverts commit e5a0d5280dfc19c21bd227cbaa4bd10f04a6c1fd.

--- Cabal-syntax/src/Distribution/Fields/Field/Typed.hs
+++ Cabal-syntax/src/Distribution/Fields/Field/Typed.hs
@@ -4,7 +4,7 @@ module Distribution.Fields.Field.Typed where
 import Prelude ()
 import Distribution.Compat.Prelude
 
-import Distribution.Fields.Field ( Name, SectionArg, FieldLine )
+import Distribution.Fields.Field ( Name, SectionArg, FieldLine , FieldName, getName )
 import Distribution.CabalSpecVersion (CabalSpecVersion)
 import Distribution.Annotation
 import Distribution.Types.Dependency
@@ -21,14 +21,34 @@ data TField ann
   | MkTSection !(Name ann) [SectionArg ann] [TField ann]
   deriving (Show)
 
-getCabalVersionField :: [TField ann] -> [(Name ann, Annotated CabalSpecVersion)]
-getCabalVersionField = mapMaybe $ \case { (MkCabalVersionTField name x) -> Just (name, x); _ -> Nothing; }
+getTFieldName :: TField ann -> Name ann
+getTFieldName (MkRawTField name _) = name
+getTFieldName (MkCabalVersionTField name _) = name
+getTFieldName (MkTargetBuildDependsTField name _) = name
+getTFieldName (MkPkgVersionTField name _) = name
+getTFieldName (MkBuildToolsTField name _) = name
+getTFieldName (MkTSection {}) = error "getTFieldName: expecting a field, got a section"
 
-getTargetBuildDependsField :: [TField ann] -> [(Name ann, AnnotatedList Dependency)]
-getTargetBuildDependsField = mapMaybe $ \case { (MkTargetBuildDependsTField name x) -> Just (name, x); _ -> Nothing; }
+-- I don't have a better solution than to use partial functions in field grammar at the moment.
+getTFieldByName :: FieldName -> [TField ann] -> [TField ann]
+getTFieldByName name0 = mapMaybe go
+  where
+    go tfield@(MkRawTField name _) | getName name == name0 = Just tfield
+    go tfield@(MkCabalVersionTField name _) | getName name == name0 = Just tfield
+    go tfield@(MkTargetBuildDependsTField name _) | getName name == name0 = Just tfield
+    go tfield@(MkPkgVersionTField name _) | getName name == name0 = Just tfield
+    go tfield@(MkBuildToolsTField name _) | getName name == name0 = Just tfield
+    go (MkTSection {}) = error "getTFieldName: expecting a field, got a section"
+    go _ = Nothing
 
-getPkgVersionField :: [TField ann] -> [(Name ann, Annotated Version)]
-getPkgVersionField = mapMaybe $ \case { (MkPkgVersionTField name x) -> Just (name, x); _ -> Nothing; }
-
-getBuildToolsField :: [TField ann] -> [(Name ann, AnnotatedList LegacyExeDependency)]
-getBuildToolsField = mapMaybe $ \case { (MkBuildToolsTField name x) -> Just (name, x); _ -> Nothing; }
+-- getCabalVersionField :: [TField ann] -> [(Name ann, Annotated CabalSpecVersion)]
+-- getCabalVersionField = mapMaybe $ \case { (MkCabalVersionTField name x) -> Just (name, x); _ -> Nothing; }
+--
+-- getTargetBuildDependsField :: [TField ann] -> [(Name ann, AnnotatedList Dependency)]
+-- getTargetBuildDependsField = mapMaybe $ \case { (MkTargetBuildDependsTField name x) -> Just (name, x); _ -> Nothing; }
+--
+-- getPkgVersionField :: [TField ann] -> [(Name ann, Annotated Version)]
+-- getPkgVersionField = mapMaybe $ \case { (MkPkgVersionTField name x) -> Just (name, x); _ -> Nothing; }
+--
+-- getBuildToolsField :: [TField ann] -> [(Name ann, AnnotatedList LegacyExeDependency)]
+-- getBuildToolsField = mapMaybe $ \case { (MkBuildToolsTField name x) -> Just (name, x); _ -> Nothing; }
